7e611b1ed5d419d224bed578929002850a9a08e2
dacthai2807 <nguyendacthai2807@gmail.com>
2023-11-21 00:58:19 +0900

    update

--- shell/train_UncerBBDM_edges2shoes.sh
+++ shell/train_UncerBBDM_edges2shoes.sh
@@ -1,6 +1,6 @@
 # train
-python3 main.py --config configs/edges2shoes.yaml --train --sample_at_start --save_top --gpu_ids 1
-# python3 main.py --config configs/UncerBBDM.yaml --sample_to_eval --gpu_ids 2 --resume_model results/UncerBBDM_1Unet_confloss_15k_v1/LBBDM-f4/checkpoint/top_model_epoch_88.pth  --resume_optim results/UncerBBDM_1Unet_confloss_15k_v1/LBBDM-f4/checkpoint/top_optim_sche_epoch_88.pth
+# python3 main.py --config configs/edges2shoes.yaml --train --sample_at_start --save_top --gpu_ids 1
+python3 main.py --config configs/edges2shoes.yaml --sample_to_eval --gpu_ids 2 --resume_model results/UncerBBDM_1Unet_confloss_edges2shoes_v1/LBBDM-f4/checkpoint/top_model_epoch_62.pth  --resume_optim results/UncerBBDM_1Unet_confloss_edges2shoes_v1/LBBDM-f4/checkpoint/top_optim_sche_epoch_62.pth
 
 # test
 # python3 main.py --config configs/LBBDM_7_21.yaml --sample_to_eval --gpu_ids 2 --resume_model results/108_CT2PET_7_17/LBBDM-f4/checkpoint/last_model.pth --resume_optim results/108_CT2PET_7_17/LBBDM-f4/checkpoint/last_optim_sche.pth
