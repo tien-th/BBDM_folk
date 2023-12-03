1806377da9dd6feabcda2961c8336a298fef9b54
dacthai2807 <nguyendacthai2807@gmail.com>
2023-12-03 17:26:49 +0900

    update

--- shell/train_UncerBBDM.sh
+++ shell/train_UncerBBDM.sh
@@ -1,7 +1,7 @@
 # train
-python3 main.py --config configs/UncerBBDM.yaml --train --sample_at_start --save_top --gpu_ids 0
+# python3 main.py --config configs/UncerBBDM.yaml --train --sample_at_start --save_top --gpu_ids 0
 # python3 main.py --config configs/UncerBBDM3c.yaml --train --sample_at_start --save_top --gpu_ids 3
-# python3 main.py --config configs/UncerBBDM.yaml --sample_to_eval --gpu_ids 0 --resume_model results/UncerBBDM_1Unet_confloss_15k_v1/LBBDM-f4/checkpoint/top_model_epoch_88.pth  --resume_optim results/UncerBBDM_1Unet_confloss_15k_v1/LBBDM-f4/checkpoint/top_optim_sche_epoch_88.pth
+python3 main.py --config configs/UncerBBDM.yaml --sample_to_eval --gpu_ids 0 --resume_model results/1130_UncerBBDM_1Unet_confloss_15k/LBBDM-f4/checkpoint/top_model_epoch_90.pth  --resume_optim /results/1130_UncerBBDM_1Unet_confloss_15k/LBBDM-f4/checkpoint/top_optim_sche_epoch_90.pth
 # python3 main.py --config configs/UncerBBDM3c.yaml --sample_to_eval --gpu_ids 3 --resume_model results/108_CT2PET_UncerBBDM3c_/LBBDM-f4/checkpoint/top_model_epoch_126.pth  --resume_optim results/108_CT2PET_UncerBBDM3c_/LBBDM-f4/checkpoint/top_optim_sche_epoch_126.pth
 
 #test
